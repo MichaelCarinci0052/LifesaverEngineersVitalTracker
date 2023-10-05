ed067075abd986536f13d6262f63827189350514
PatrikPrenga8752 <patrikprenga21@gmail.com>
2023-10-05 03:20:59 -0400

    changed the nav drawer to bottom nav drawer for better user experience, also added a home drawer with app icon. added app icon to splash screen

--- app/src/main/java/ca/lifesaver/engineers/it/vital/tracker/MainActivity.java
+++ app/src/main/java/ca/lifesaver/engineers/it/vital/tracker/MainActivity.java
@@ -1,18 +1,16 @@
+
 package ca.lifesaver.engineers.it.vital.tracker;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.os.Bundle;
 import android.view.MenuItem;
-import com.google.android.material.navigation.NavigationView;
 
 /**
  * Jason Macdonald N01246828 section: 0CB
@@ -20,40 +18,27 @@ import com.google.android.material.navigation.NavigationView;
  * Patrik Prenga n01428752  section: 0CB
  * Nicholas Rafuse n01440073 section: 0CB
  */
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         this.configureToolBar();
-        this.configureDrawerLayout();
-        this.configureNavigationView();
+        this.configureBottomNavigationView();
     }
 
     @Override
     public void onBackPressed() {
-
-        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            this.drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            showExitConfirmationDialog();
-        }
+        showExitConfirmationDialog();
     }
 
     private void showExitConfirmationDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-
         builder.setIcon(R.drawable.baseline_heart_broken_24);
-
         builder.setTitle(R.string.confirmexit);
         builder.setMessage(R.string.areyousure);
         builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
@@ -77,43 +62,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
     }
 
-    private void configureDrawerLayout(){
-        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-    }
-
-    private void configureNavigationView(){
+    private void configureBottomNavigationView(){
         FragmentManager fragmentManager = getSupportFragmentManager();
-        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.activity_main_drawer_account:
-                        AccountFragment account = new AccountFragment();
-                        fragmentManager.beginTransaction().replace(R.id.activity_main_frame_layout, account).commit();
-                        break;
-
-                    case R.id.activity_main_drawer_gps:
-                        GPSFragment gps = new GPSFragment();
-                        fragmentManager.beginTransaction().replace(R.id.activity_main_frame_layout, gps).commit();
-                        break;
-
-                    case R.id.activity_main_drawer_vitals:
-                        VitalsFragment vitals = new VitalsFragment();
-                        fragmentManager.beginTransaction().replace(R.id.activity_main_frame_layout, vitals).commit();
-                        break;
-                }
-                return false;
+        this.bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.activity_main_drawer_account:
+                    AccountFragment account = new AccountFragment();
+                    fragmentManager.beginTransaction().replace(R.id.activity_main_frame_layout, account).commit();
+                    break;
+                case R.id.activity_main_drawer_gps:
+                    GPSFragment gps = new GPSFragment();
+                    fragmentManager.beginTransaction().replace(R.id.activity_main_frame_layout, gps).commit();
+                    break;
+                case R.id.activity_main_drawer_vitals:
+                    VitalsFragment vitals = new VitalsFragment();
+                    fragmentManager.beginTransaction().replace(R.id.activity_main_frame_layout, vitals).commit();
+                    break;
             }
+            return true;
         });
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
     }
 }
