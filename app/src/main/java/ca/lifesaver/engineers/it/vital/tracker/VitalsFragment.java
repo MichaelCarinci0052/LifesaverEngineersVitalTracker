7d18eb0122c9c5f977e31cb9de3ed1f2c0bc9858
Jason <jasonmac644@gmail.com>
2023-10-09 19:00:13 -0400

    Implemented random data for sensors

--- app/src/main/java/ca/lifesaver/engineers/it/vital/tracker/VitalsFragment.java
+++ app/src/main/java/ca/lifesaver/engineers/it/vital/tracker/VitalsFragment.java
@@ -1,50 +1,30 @@
 package ca.lifesaver.engineers.it.vital.tracker;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.os.Handler;
+import java.util.Random;
+import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VitalsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-
-
-/**
- * Jason Macdonald N01246828 section: 0CB
- * Michael Carinci n01480052 section: 0CB
- * Patrik Prenga n01428752  section: 0CB
- * Nicholas Rafuse n01440073 section: 0CB
- */
 public class VitalsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private Handler handler;
+    private Runnable updateRunnable;
+    private Random random;
     public VitalsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VitalsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static VitalsFragment newInstance(String param1, String param2) {
         VitalsFragment fragment = new VitalsFragment();
         Bundle args = new Bundle();
@@ -61,6 +41,8 @@ public class VitalsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        handler = new Handler();
+        random = new Random();
     }
 
     @Override
@@ -68,5 +50,44 @@ public class VitalsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vitals, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Reference the TextViews
+        TextView tvHeartRate = view.findViewById(R.id.heartRateValue);
+        TextView tvOxygenLevel = view.findViewById(R.id.oxygenValue);
+        TextView tvBodyTemp = view.findViewById(R.id.bodyTempValue);
+
+        // Set dummy data
+        tvHeartRate.setText("Heart Rate: 80 BPM");
+        tvOxygenLevel.setText("Oxygen Level: 98%");
+        tvBodyTemp.setText("Body Temperature: 98.6°F");
+        updateRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // Generate random data
+                int heartRate = 60 + random.nextInt(40);  // Random value between 60 and 100
+                int oxygenLevel = 90 + random.nextInt(10);  // Random value between 90 and 100
+                float bodyTemp = 97.0f + random.nextFloat() * 3.0f;  // Random value between 97.0 and 100.0
+
+                // Update the UI
+                tvHeartRate.setText("Heart Rate: " + heartRate + " BPM");
+                tvOxygenLevel.setText("Oxygen Level: " + oxygenLevel + "%");
+                tvBodyTemp.setText(String.format("Body Temperature: %.1f°F", bodyTemp));
+
+                // Schedule the next update
+                handler.postDelayed(this, 2000);  // Update every 2 seconds
+            }
+        };
+
+        // Start the updates
+        handler.post(updateRunnable);
+    }
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Stop the updates when the fragment is destroyed
+        handler.removeCallbacks(updateRunnable);
     }
 }
