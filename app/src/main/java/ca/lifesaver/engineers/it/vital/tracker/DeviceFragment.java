ff3b8ec839682a0bec786092ce94d62c225841ea
PatrikPrenga8752 <patrikprenga21@gmail.com>
2023-10-10 14:53:26 -0400

    created "simulated" functionality for the device container by adding a battery perctage and button to turn the device on and off

--- app/src/main/java/ca/lifesaver/engineers/it/vital/tracker/DeviceFragment.java
+++ app/src/main/java/ca/lifesaver/engineers/it/vital/tracker/DeviceFragment.java
@@ -1,64 +1,39 @@
 package ca.lifesaver.engineers.it.vital.tracker;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DeviceFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ca.lifesaver.engineers.it.vital.tracker.R;
+
 public class DeviceFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DeviceFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DeviceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DeviceFragment newInstance(String param1, String param2) {
-        DeviceFragment fragment = new DeviceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_device, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_device, container, false);
+        // Find the button and set up the click listener
+        Button toggleDeviceButton = view.findViewById(R.id.toggleDeviceButton);
+        toggleDeviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String currentText = toggleDeviceButton.getText().toString();
+                if ("Turn On".equals(currentText)) {
+                    toggleDeviceButton.setText("Turn Off");
+                    // Logic to turn the device ON goes here
+                } else {
+                    toggleDeviceButton.setText("Turn On");
+                    // Logic to turn the device OFF goes here
+                }
+            }
+        });
+
+        return view;
     }
 }
