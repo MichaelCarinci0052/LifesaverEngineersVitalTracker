25ca9848ad46b6677cd71f680496d47cad177368
Jason <jasonmacdonald644@yahoo.com>
2023-10-12 18:32:15 -0400

    Updated UI of login screen

--- app/src/main/java/ca/lifesaver/engineers/it/vital/tracker/LoginActivity.java
+++ app/src/main/java/ca/lifesaver/engineers/it/vital/tracker/LoginActivity.java
@@ -5,16 +5,23 @@ import static java.security.AccessController.getContext;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.common.SignInButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -29,6 +36,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText editTextUsername;
     private EditText editTextPassword;
+    private TextInputLayout passwordLayout;
+    private TextInputLayout emailLayout;
     private Button buttonLogin;
     private FirebaseAuth mAuth;
     private SharedViewModal viewModel;
@@ -37,10 +46,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         viewModel = new ViewModelProvider(this).get(SharedViewModal.class);
-
+        passwordLayout = findViewById(R.id.passwordLayout);
+        emailLayout = findViewById(R.id.emailLayout);
         mAuth = FirebaseAuth.getInstance();
 
-
+        //google Sign in button
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setColorScheme(0);
+        signInButton.setSize(1);
 
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -49,46 +62,91 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = editTextUsername.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
-
-
-                //Firebase initialization
-                mAuth.signInWithEmailAndPassword(username, password)
-                        .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                // Sign in success
-                                SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-                                editor.putString("username", username);
-                                editor.apply();
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                // Sign in failed
-                                Toast.makeText(LoginActivity.this, "Invalid credentials!", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-//                if ("password123".equals(password)) {
-//                    // Successful login
-//
-//                    // Save the username to SharedPreferences
-//                    SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
-//                    SharedPreferences.Editor editor = sharedPreferences.edit();
-//                    editor.putString("username", username);
-//                    editor.apply();
-//
-//                    // Navigate to the MainActivity
-//                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                    startActivity(intent);
-//                    finish();
-//                } else {
-//                    Toast.makeText(LoginActivity.this, "Invalid credentials!", Toast.LENGTH_SHORT).show();
-//                }
+                if(isValidEmail()&&isValidPassword()){
+                    String username = editTextUsername.getText().toString().trim();
+                    String password = editTextPassword.getText().toString().trim();
+                    //Firebase initialization
+                    mAuth.signInWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    // Sign in success
+                                    SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("username", username);
+                                    editor.apply();
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // Sign in failed
+                                    Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Authentication Failed!", Snackbar.LENGTH_SHORT);
+                                    View snackbarView = snackbar.getView();
+                                    FrameLayout.LayoutParams params =(FrameLayout.LayoutParams)snackbarView.getLayoutParams();
+                                    params.gravity = Gravity.TOP;
+                                    snackbarView.setLayoutParams(params);
+                                    snackbar.show();
+                                }
+                            });
+                }
 
             }
         });
+        editTextUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing here
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Do nothing here
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                isValidEmail();
+            }
+        });
+
+        editTextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Do nothing here
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Do nothing here
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                isValidPassword();
+            }
+        });
+
 
     }
+    private boolean isValidEmail() {
+        if (editTextUsername.length() == 0) {
+            emailLayout.setError("This field is required");
+            return false;
+        }
+        emailLayout.setError(null); // Clear error
+        return true;
+    }
+
+    private boolean isValidPassword() {
+        if (editTextPassword.length() == 0) {
+            passwordLayout.setError("Password is required");
+            return false;
+        } else if (editTextPassword.length() < 8) {
+            passwordLayout.setError("Password must be minimum 8 characters");
+            return false;
+        }
+        passwordLayout.setError(null); // Clear error
+        return true;
+    }
+
+
 }
